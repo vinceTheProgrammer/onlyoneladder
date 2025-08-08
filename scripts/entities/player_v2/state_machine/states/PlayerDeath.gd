1100b5d3ddc2349c99e6be9d83b49811ea771d20
car <samchong2005@icloud.com>
2025-08-08 11:24:24 +0800

    minor death duration variable tweak
    -variable name converted to lowercap
    -death duration changed from constant to variable.
    -exported death_duration, allowing  the value to be edited in the Inspector panel.
    -added description for the variable

--- scripts/entities/player_v2/state_machine/states/PlayerDeath.gd
+++ scripts/entities/player_v2/state_machine/states/PlayerDeath.gd
@@ -4,7 +4,7 @@ class_name PlayerDeath
 
 @onready var ThisPlayer = get_parent().playerx
 
-const DEATH_DURATION: int = 1
+@export var death_duration: float = 1 ##The amount of time it takes to restart the level after a player dies (in seconds).
 
 func State_Enter():
 	ThisPlayer.anims.play("Death")
@@ -12,7 +12,7 @@ func State_Enter():
 
 
 func State_Physics_Update(delta): 
-	await get_tree().create_timer(DEATH_DURATION).timeout
+	await get_tree().create_timer(death_duration).timeout
 	get_tree().reload_current_scene()
 	
 	
